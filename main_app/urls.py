c2520385408ab58007ab168ea6063c6ef07f0e7d
unknown <marshallsimcox@gmail.com>
2021-04-20 14:17:59 -0600

    Add forms.py and corresponding code

--- main_app/urls.py
+++ main_app/urls.py
@@ -18,6 +18,6 @@ from main_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^([0-9]+)/$',
-        views.detail, name = 'detail'),
+    url(r'^([0-9]+)/$', views.detail, name = 'detail'),
+    url(r'^post_url/$', views.post_treasure, name='post_treasure'),
 ]
