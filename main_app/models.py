b11c99a8a680dcac6583c71d7d04de9bf9426cd4
msimcox <msimcox84@gmail.com>
2021-04-21 18:34:56 -0600

    Add user to models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+# If the user is deleted, the object will update the user to "deleted". Alternative is cascade delete, which would delete the object when the user is deleted.
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 class Treasure(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user),)
     name = models.CharField(max_length=100)
     value = models.DecimalField(max_digits=10, decimal_places=2)
     material = models.CharField(max_length=100)
